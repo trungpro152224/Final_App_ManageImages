a1c140038cf317b6fdb9ee84c88f08a58a18cb04
TRUNG <trungpro151224@gmail.com>
2018-01-18 00:47:45 +0700

    create login with gem devise

--- Final_App_ManageImages/app/controllers/application_controller.rb
+++ Final_App_ManageImages/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    super resource
+  end
+  def after_existing_sign_in_path_for(resource)
+  dashboard_path
+end
 end
---
+++ Final_App_ManageImages/app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ApplicationController
+	 before_action :authenticate_user!
+  def index
+  # user_signed_in?
+  end
+end
