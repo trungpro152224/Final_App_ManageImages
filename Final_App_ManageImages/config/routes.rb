781b961d98f22f95f7350fd38a175185cd9eb1c1
TRUNG <trungpro151224@gmail.com>
2018-01-21 15:18:00 +0700

    feature login is successful

--- Final_App_ManageImages/config/routes.rb
+++ Final_App_ManageImages/config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'welcome/index'
+  root 'welcome#index'
   resources :pictures
 
   authenticated :user do 
   	root 'pictures#index', as: "authenticated_root"
   end
 
-  root 'welcome#index'
- 
+  
+
 end
